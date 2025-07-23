b0f36d9278573813c50bff66f2f1ce195bff4218
SmallKnight2314 <84006239+SmallKnight2314@users.noreply.github.com>
2025-07-23 15:05:35 +0200

    Update it_ticket_bot.py

--- telegram-bot-github/src/bot/it_ticket_bot.py
+++ telegram-bot-github/src/bot/it_ticket_bot.py
@@ -1,40 +1,59 @@
 # it_ticket_bot.py
+import logging
 from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters
 from src.dialog.bot_dialog import BotDialog, States
 from src.data.form_data import FormData
 from src.services.email_service import EmailService
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 class ITTicketBot:
     def __init__(self, token, smtp_server, smtp_port, email_sender, email_password, email_recipient):
+        logger.debug(f"Initializing ITTicketBot with token: {token[:5]}...")
+        if not token:
+            logger.error("TELEGRAM_TOKEN is missing or invalid")
+            raise ValueError("TELEGRAM_TOKEN is required")
         self.token = token
         self.email_service = EmailService(smtp_server, smtp_port, email_sender, email_password, email_recipient)
         self.form_data = FormData()
         self.dialog = BotDialog(self.form_data, self.email_service)
         self.application = Application.builder().token(self.token).build()
+        logger.debug("ITTicketBot initialized")
 
     def setup_handlers(self):
+        logger.debug("Setting up handlers")
         conv_handler = ConversationHandler(
             entry_points=[CommandHandler('start', self.dialog.start)],
             states={
-                States.SUBJECT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_subject)],
-                States.OTHER_SUBJECT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_other_subject)],
-                States.NAME.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_name)],
-                States.DEPARTMENT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_department)],
-                States.OTHER_DEPARTMENT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_other_department)],
-                States.CAMPUS.value: [MessageHandler(filters.TEXT & ~filters.COMMAND | filters.LOCATION, self.dialog.get_campus)],
-                States.OTHER_CAMPUS.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_other_campus)],
-                States.BUILDING.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_building)],
-                States.FLOOR.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_floor)],
-                States.ROOM.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_room)],
-                States.PHONE.value: [MessageHandler(filters.CONTACT | filters.TEXT & ~filters.COMMAND, self.dialog.get_phone)],
-                States.EMAIL.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_email)],
-                States.DESCRIPTION.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_description)],
-                States.CALLBACK.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.get_callback)],
+                States.CATEGORY.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.category)],
+                States.COMPONENT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.component)],
+                States.ISSUE.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.issue)],
+                States.OTHER_ISSUE.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.other_issue)],
+                States.CAMPUS.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.campus)],
+                States.WARD.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.ward)],
+                States.DEPARTMENT.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.department)],
+                States.NAME.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.name)],
+                States.PHONE.value: [
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.phone),
+                    MessageHandler(filters.CONTACT, self.dialog.phone)
+                ],
+                States.EMAIL.value: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.email)],
+                States.DESCRIPTION.value: [
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.dialog.description),
+                    MessageHandler(filters.Regex('^Skip$'), self.dialog.description)
+                ]
             },
-            fallbacks=[CommandHandler('cancel', self.dialog.cancel)]
+            fallbacks=[
+                CommandHandler('cancel', self.dialog.cancel),
+                MessageHandler(filters.Regex('^PANIC$'), self.dialog.panic)
+            ]
         )
         self.application.add_handler(conv_handler)
+        logger.info("Bot handlers set up successfully")
 
     def run(self):
+        logger.debug("Starting bot polling")
         self.setup_handlers()
         self.application.run_polling()
+        logger.debug("Bot polling ended")
