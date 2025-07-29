a80efc3e00e1c4565d3e456c4a40557142b9a571
SmallKnight2314 <84006239+SmallKnight2314@users.noreply.github.com>
2025-07-29 16:14:45 +0200

    Create states.py
    new file to help handle states

---
+++ telegram-bot-github/src/states.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class States(Enum):
+    CATEGORY = 1
+    COMPONENT = 2
+    ISSUE = 3
+    OTHER_ISSUE = 4
+    CAMPUS = 5
+    DEPARTMENT = 6
+    ROOM = 7
+    NAME = 8
+    PHONE = 9
+    EMAIL = 10
+    DESCRIPTION = 11
